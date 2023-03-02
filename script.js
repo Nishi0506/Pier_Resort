d1ec62e4b96b79c88121681350bb6e0ebbf5c6cb
Nishi0506 <126757528+Nishi0506@users.noreply.github.com>
2023-03-02 20:33:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+//datepicker
+$(function () {
+  $("#datepicker").datepicker();
+});
+$(function () {
+  $("#datepicker1").datepicker();
+});
+
+//readmore
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Read More") {
+    readMoreBtn.innerText = "Read Less";
+  } else {
+    readMoreBtn.innerText = "Read More";
+  }
+});
+
+//slide
+let items = document.querySelectorAll(".carousel .carousel-item");
+
+items.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
